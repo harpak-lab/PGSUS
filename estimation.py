e076d5a96313c6d6c4726d16b74162c7f4608661
smithsap <35776133+smithsap@users.noreply.github.com>
2024-09-05 14:53:52 -0500

    Added updated scripts

---
+++ estimation.py
@@ -0,0 +1,233 @@
+import numpy as np
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=RuntimeWarning)
+import pandas as pd
+import sys
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+from permutations import block_permutation
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from pylr2 import regress2
+from scipy.odr import *
+
+class estimate_components(object):
+
+    def __init__(self, pc_genotypes, gwas_beta, gwas_se, sib_beta, sib_se, chr_pos, asc_p, thresh, outpath, outlabel, pos_label,
+        pc_lower_bound=100, eigenvecs= None, eigenvalues = None, boot_se = 100, block_perm = True, pcs_to_test = 15, nperm = 1000):
+        
+        self.gwas_beta = gwas_beta.reset_index(drop = True)
+        self.gwas_se = gwas_se.reset_index(drop = True)
+        self.sib_beta = sib_beta.reset_index(drop = True)
+        self.sib_se = sib_se.reset_index(drop = True)
+        self.ascertainment_p = asc_p.reset_index(drop = True)
+        self.thresh = thresh
+        self.pc_lower_bound = pc_lower_bound
+        self.outpath = outpath
+        self.nboot = boot_se
+        self.nperm = nperm
+        self.pcs_to_test = pcs_to_test
+        self.outlabel = outlabel
+        self.plot = 0
+
+        if eigenvecs == None:
+            self.pca(pc_genotypes)
+        else:
+            self.eigenvalues = np.load(eigenvalues)
+            self.eigenvecs = np.load(eigenvecs)
+
+        self.alpha = 1.
+        self.pc_upper_bound = self.eigenvalues.shape[0]
+        self.direct_variance_component_init, self.sad_variance_component_init, self.covar_variance_component_init, self.decomp_gwas, self.decomp_sib, self.decomp_diff, self.gwas_avg_se2,self.sib_avg_se2, self.proj_gwas, self.proj_sib, self.proj_diff, self.variance_direct_vc, self.variance_sad_vc, self.variance_covar_vc, self.standard_variance_component, self.gwas_beta_threshed, self.gwas_se_threshed, self.sib_beta_threshed, self.sib_se_threshed, self.beta_sum, self.alpha, self.alpha_se, self.vartotals_init, self.tau = self.estimate_components_and_alpha(self.gwas_beta, self.gwas_se,self.sib_beta,self.sib_se,self.ascertainment_p, self.thresh,self.eigenvecs, self.eigenvalues, 1, 1, self.pc_upper_bound, pc_lower_bound = self.pc_lower_bound)
+        self.alpha_report = self.alpha
+        print(self.outlabel)
+        print(self.thresh)
+        print('first:' + str(self.alpha_report) + '(' + str(self.alpha_se) + ')')
+
+        self.gwas_beta = gwas_beta.reset_index(drop = True)/self.alpha_report
+        self.gwas_se = gwas_se.reset_index(drop = True)/self.alpha_report
+
+        self.sib_beta = sib_beta.reset_index(drop = True)
+        self.sib_se = sib_se.reset_index(drop = True)
+        self.pc_upper_bound = self.eigenvalues.shape[0]
+        self.ascertainment_p = asc_p.reset_index(drop = True)
+        self.thresh = thresh
+        self.pc_lower_bound = pc_lower_bound
+        
+        self.direct_variance_component, self.sad_variance_component, self.covar_variance_component, self.decomp_gwas, self.decomp_sib, self.decomp_diff, self.gwas_avg_se2,self.sib_avg_se2, self.proj_gwas, self.proj_sib, self.proj_diff, self.variance_direct_vc, self.variance_sad_vc, self.variance_covar_vc, self.standard_variance_component, self.gwas_beta_threshed, self.gwas_se_threshed, self.sib_beta_threshed, self.sib_se_threshed, self.beta_sum, self.alpha, self.alpha_se_new, self.vartotals, self.tau = self.estimate_components_and_alpha(self.gwas_beta, self.gwas_se,self.sib_beta,self.sib_se, self.ascertainment_p, self.thresh,self.eigenvecs, self.eigenvalues, 0, self.tau, self.pc_upper_bound, pc_lower_bound = self.pc_lower_bound)
+        
+        if block_perm:
+            block_permutation(chr_pos, self.gwas_beta_threshed, self.gwas_se_threshed, self.sib_beta_threshed, self.sib_se_threshed, self.ascertainment_p, self.thresh, outlabel, self.eigenvecs, self.eigenvalues, self.direct_variance_component, self.sad_variance_component, self.covar_variance_component, self.decomp_gwas, self.decomp_sib, self.decomp_diff,self.variance_direct_vc, self.variance_sad_vc, self.variance_covar_vc, outpath, pos_label, self.pcs_to_test, nperm = nperm)
+        else:
+            pass
+
+    def element_multiplier(self,x,y):
+        return np.multiply(x,y)
+
+    def prs_decomp(self,eff_sizes,eigenvalues,eigenvecs):
+        eff_sizes = np.array(eff_sizes).reshape(self.eigenvecs.shape[0])
+        temp = np.apply_along_axis(self.element_multiplier,0,eigenvecs,eff_sizes)
+        temp = np.sum(temp, axis = 0)
+        temp = np.power(temp,2)
+        temp = np.multiply(eigenvalues,temp)
+        return temp
+        
+    def error_decomp(self,ses,eigenvalues,eigenvecs):
+        ses = np.power(np.array(ses).reshape(self.eigenvecs.shape[0]),2)
+        temp = np.apply_along_axis(self.element_multiplier, 0, np.power(eigenvecs,2), ses)
+        temp = np.sum(temp, axis = 0)
+        temp = np.multiply(eigenvalues,temp)
+        return temp
+
+    def deming_error_decomp(self,errors,eigenvalues,eigenvecs):
+        errors = np.array(errors).reshape(self.eigenvecs.shape[0])
+        temp = np.sum(np.apply_along_axis(self.element_multiplier, 0, np.power(eigenvecs,2), errors),axis = 0)
+        temp = np.power(temp,2)
+        temp = 2*np.multiply(np.power(eigenvalues,2),temp)
+        return temp
+
+    def prs_decomp_unsq(self,eff_sizes,eigenvalues,eigenvecs):
+        eff_sizes = np.array(eff_sizes).reshape(self.eigenvecs.shape[0])
+        temp = np.apply_along_axis(self.element_multiplier, 0, eigenvecs,eff_sizes)
+        temp = np.sum(temp, axis = 0)
+        temp = np.multiply(eigenvalues,temp)
+        return temp
+
+    def beta_p_thresh(self,betas,p,thresh):
+        filt_p = p[p >= thresh].index.tolist()
+        self.ascertained_snp_index = p[p < thresh].index.tolist()
+        betas.loc[betas.index.isin(filt_p)] = 0
+        self.nsnp = betas.shape[0] - len(filt_p)
+        return betas
+
+    def se2_avg_p_thresh(self,ses,p,thresh):
+        filt_p = p[p >= thresh].index.tolist()
+        return np.mean(ses.loc[~ses.index.isin(filt_p)]**2)
+
+    def f(self,B,x):
+        return B[0]*x
+
+    def se_bootstrapper(self,lmdf):
+        linear = Model(self.f)
+        estimates = []
+        for strap in range(100):
+            indices = np.random.choice(lmdf.index.tolist(), lmdf.shape[0])
+            temp_lmdf = lmdf.loc[indices]
+            mydata = RealData(x=temp_lmdf['sib_vc'],y=temp_lmdf['standard_vc'], sx = temp_lmdf['var_sib_vc'], sy = temp_lmdf['var_standard_vc'])
+            myodr = ODR(mydata, linear, beta0 = [0.])
+            myoutput = myodr.run()
+            estimates.append(np.sqrt(np.abs(myoutput.beta[0])))
+        return np.std(estimates)
+
+
+    def estimate_components_and_alpha(self,gwas_beta,gwas_se,sib_beta,sib_se,ascertainment_p,thresh,eigenvecs,eigenvalues, plotter, tau,
+        pc_upper_bound, pc_lower_bound = 100):
+        
+        #get all necessary statistics from the population gwas
+        #get gwas effects that have p values less than threshold
+        gwas_beta_threshed = self.beta_p_thresh(gwas_beta, ascertainment_p, thresh)
+        #get gwas SEs that have p values less than threshold
+        gwas_se_threshed = self.beta_p_thresh(gwas_se, ascertainment_p, thresh)
+        #get gwas SEs that have p values less than threshold
+        gwas_avg_se2 = self.se2_avg_p_thresh(gwas_se, ascertainment_p, thresh)
+        
+        #get all necessary statistics from the sib gwas
+        #get sib effects that have p values less than threshold
+        sib_beta_threshed = self.beta_p_thresh(sib_beta, ascertainment_p, thresh)
+        #get sib effects that have p values less than threshold
+        sib_se_threshed = self.beta_p_thresh(sib_se, ascertainment_p, thresh)
+
+        #get sib SEs that have p values less than threshold
+        sib_avg_se2 = self.se2_avg_p_thresh(sib_se, ascertainment_p, thresh)
+        #perform the decomposition for each component of interest       
+        decomp_gwas = self.prs_decomp(gwas_beta_threshed,eigenvalues,eigenvecs)
+        decomp_sib = self.prs_decomp(sib_beta_threshed,eigenvalues,eigenvecs)
+        decomp_diff = self.prs_decomp(gwas_beta_threshed - sib_beta_threshed, eigenvalues, eigenvecs)
+
+        #perform the decomposition for each component of interest, unsquared
+        proj_gwas = self.prs_decomp_unsq(gwas_beta_threshed,eigenvalues,eigenvecs)
+        proj_sib = self.prs_decomp_unsq(sib_beta_threshed,eigenvalues,eigenvecs)
+        proj_diff = self.prs_decomp_unsq(gwas_beta_threshed - sib_beta_threshed,eigenvalues,eigenvecs)
+        
+        #decompose the standard errors of the gwas and sibling standard errors
+        decomp_gwas_se = self.error_decomp(gwas_se_threshed, eigenvalues, eigenvecs)
+        decomp_sib_se = self.error_decomp(sib_se_threshed, eigenvalues, eigenvecs)
+        direct_variance_component = decomp_sib - decomp_sib_se
+        sad_variance_component = decomp_diff - decomp_sib_se - decomp_gwas_se
+        covar_variance_component = decomp_gwas - decomp_diff - decomp_sib + 2*decomp_sib_se
+        standard_variance_component = decomp_gwas - decomp_gwas_se
+        
+        #get variance of each variance component
+        variance_direct_vc = 2*(decomp_sib_se**2)
+        variance_sad_vc = 2*((decomp_sib_se+decomp_gwas_se)**2)
+        variance_covar_vc = 4*(decomp_sib_se+decomp_gwas_se) + 8*(decomp_sib_se**2)
+                
+        variance_standard_vc = np.power(2*((decomp_gwas_se)**2),0.5)
+        variance_sib_vc = np.power(2*((decomp_sib_se)**2),0.5)
+
+        startdf = np.vstack((standard_variance_component,direct_variance_component,variance_standard_vc,variance_sib_vc))
+        lmdf = pd.DataFrame(data=startdf, index = ['standard_vc','sib_vc','var_standard_vc','var_sib_vc']).T
+        lmdf = lmdf.astype(float)
+        lmdf = lmdf.iloc[pc_lower_bound:pc_upper_bound]
+        
+        linear = Model(self.f)
+        mydata = RealData(x=lmdf['sib_vc'],y=lmdf['standard_vc'],sx = lmdf['var_sib_vc'], sy = lmdf['var_standard_vc'])
+        myodr = ODR(mydata, linear, beta0 = [0.])
+        myoutput = myodr.run()
+        alpha = np.sqrt(np.abs(myoutput.beta[0]))
+        alpha_se = self.se_bootstrapper(lmdf)
+
+        if self.plot == 0:
+            fig, ax = plt.subplots(1,1,figsize = (5,5))
+            ax.scatter(lmdf['sib_vc'],lmdf['standard_vc'],color = 'grey',s = 3)
+            ax.errorbar(lmdf['sib_vc'],lmdf['standard_vc'],xerr = lmdf['var_sib_vc'],color = 'grey',elinewidth=1)
+            ax.errorbar(lmdf['sib_vc'],lmdf['standard_vc'],yerr = lmdf['var_standard_vc'],color = 'grey',elinewidth=1)
+            xseq = ax.get_xlim()
+            ax.plot(xseq,xseq,color = 'black',linestyle='--',alpha=0.75)
+            plt.tight_layout()
+            plt.savefig(self.outpath + '/' + self.outlabel +'.pval.' + str(self.thresh) + '.pdf')
+            self.plot = 1
+            
+        total_var = np.sum([np.sum(direct_variance_component),np.sum(sad_variance_component),np.sum(covar_variance_component),np.sum(decomp_gwas_se)])
+        var_props_all_pcs = np.array([np.sum(direct_variance_component),np.sum(sad_variance_component),np.sum(covar_variance_component),np.sum(decomp_gwas_se)])
+        var_props_all_pcs[:3] = var_props_all_pcs[:3]/float(np.sum(var_props_all_pcs[:3]))
+        var_props_bottom_pcs= [np.sum(direct_variance_component[self.pc_lower_bound:]),np.sum(sad_variance_component[self.pc_lower_bound:]),np.sum(covar_variance_component[self.pc_lower_bound:]),np.sum(decomp_gwas_se[self.pc_lower_bound:])]/np.sum(decomp_gwas[self.pc_lower_bound:])
+    
+        colnames = ['Direct', 'SAD', 'Direct-SAD covariance', 'Error']
+        rownames = ['All_PCs', 'PC ' + str(self.pc_lower_bound) + '+']
+
+        vartotals = pd.DataFrame(np.vstack((var_props_all_pcs,var_props_bottom_pcs)), index = rownames, columns = colnames).T
+
+        return direct_variance_component, sad_variance_component, covar_variance_component, decomp_gwas, decomp_sib, decomp_diff, gwas_avg_se2, sib_avg_se2, proj_gwas, proj_sib, proj_diff, variance_direct_vc, variance_sad_vc, variance_covar_vc, standard_variance_component, gwas_beta_threshed, gwas_se_threshed, sib_beta_threshed, sib_se_threshed, np.sum(gwas_beta_threshed[gwas_beta_threshed != 0]), alpha, alpha_se, vartotals, tau
+
+    def pca(self, genotype_mat):
+
+        pca = PCA()
+        pca.fit_transform(genotype_mat)
+        
+        self.eigenvecs = pca.components_.T
+        self.eigenvalues = pca.explained_variance_.T
+
+        if self.outlabel != None:
+            np.save(self.outpath + '/' + self.outlabel + '.eigenvectors', self.eigenvecs)
+            np.save(self.outpath + '/' + self.outlabel + '.eigenvalues', self.eigenvalues)
+        else:
+            np.save(self.outpath + '/eigenvectors', self.eigenvecs)
+            np.save(self.outpath + '/eigenvalues', self.eigenvalues)
+
+    def outputs(self):
+        return {'direct_vc':self.direct_variance_component,'sad_vc':self.sad_variance_component, 'covar_vc':self.covar_variance_component, 
+            'decomp_gwas':self.decomp_gwas, 'decomp_sib':self.decomp_sib, 'decomp_diff':self.decomp_diff,
+            'gwas_avg_se2':self.gwas_avg_se2,'sib_avg_se2':self.sib_avg_se2,
+            'proj_gwas':self.proj_gwas, 'proj_sib':self.proj_sib, 'proj_diff':self.proj_diff,
+            'var_direct_vc':self.variance_direct_vc, 'var_sad_vc':self.variance_sad_vc, 'var_covar_vc':self.variance_covar_vc,
+            'beta_sum':self.beta_sum, 'alpha':self.alpha_report,'alpha_se':self.alpha_se_new, 'nsnp':self.nsnp, 
+            'var_totals':self.vartotals_init}
+
+
+        
+
+
+
---
+++ munge_sumstats.py
@@ -0,0 +1,226 @@
+import pandas as pd 
+import numpy as np 
+import time, sys, traceback, argparse
+import os, subprocess
+from scipy import stats
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--bfile", type=str, default = 'support_files/eur_1000G.noduplicates.maf01.snpsonly', dest = 'genetic_file')
+parser.add_argument("--anc-data", type=str, default = 'support_files/SNPalleles_1000Genomes_allsites.txt.gz', dest = 'ancdata')
+parser.add_argument("--pop-gwas-file", type=str, default = './', dest = 'popgwas')
+parser.add_argument("--sib-perm-file", type=str, default = './', dest = 'sibgwasperm')
+parser.add_argument("--chr", type=str, default = 'CHR', dest = 'chrom')
+parser.add_argument("--pos", type=str, default = 'POS', dest = 'pos')
+parser.add_argument("--snp-id", type=str, default = 'SNP', dest = 'snpid')
+parser.add_argument('--outdir', type = str, dest = 'outdir')
+parser.add_argument('--outlabel', type = str, dest = 'outlabel', default = '')
+parser.add_argument('--standard-beta', type = str, dest = 'standard_beta', default = 'BETA')
+parser.add_argument('--sib-beta', type = str, dest = 'sib_beta', default = 'BETA')
+parser.add_argument('--trait', type = str, dest = 'trait')
+parser.add_argument('--pval', type = str, dest = 'P', default = 'P')
+parser.add_argument('--preselected-snps', type=str, default = None, dest = 'snpset')
+parser.add_argument('--a1', type = str, default = 'A1', dest='alt_allele')
+parser.add_argument('--p-is-log', default=False, action=argparse.BooleanOptionalAction, dest = 'log10p')
+parser.add_argument('--logp-col', default=None, dest = 'logp_col', type = str)
+
+args = parser.parse_args()
+genetic_file = args.genetic_file
+popgwas = args.popgwas
+sibgwasperm = args.sibgwasperm
+outdir = args.outdir
+outlabel = args.outlabel
+ancdata = args.ancdata
+chrom = args.chrom
+pos = args.pos
+snpid = args.snpid
+trait = args.trait
+pval = args.P
+
+alt_allele = args.alt_allele
+snpset = args.snpset
+sib_beta = args.sib_beta
+standard_beta = args.standard_beta
+log10p = args.log10p
+
+class make_input_files(object):
+
+	def __init__(self, outdir, outlabel, pop_gwas_file, sib_gwas_perm_file, genetic_file, anc_data, 
+		chr_label, pos_label, snpid, trait, alt_allele, standard_beta, sib_beta, log10p, snpset = None):
+
+		self.pop_gwas_file_name = pop_gwas_file
+		self.sib_gwas_file_name = sib_gwas_perm_file
+		self.anc_data = pd.read_csv(anc_data,sep = '\t', compression='gzip')
+		self.genetic_file = genetic_file
+		self.standard_beta = standard_beta
+		self.sib_beta = sib_beta
+		self.log10p = log10p
+
+		self.outdir = outdir
+		self.outlabel = outlabel
+		self.chr_label = chr_label
+		self.pos_label = pos_label
+		self.snpid = snpid
+		self.trait = trait
+		self.alt_allele = alt_allele
+
+		if '.gz' in self.pop_gwas_file_name:
+			self.pop_gwas = pd.read_csv(self.pop_gwas_file_name, delim_whitespace = True, compression = 'gzip')
+		else:
+			self.pop_gwas = pd.read_csv(self.pop_gwas_file_name, delim_whitespace = True)
+
+		self.pop_gwas = self.pop_gwas.rename(columns = {snpid:'SNP'})
+		
+		if '.gz' in self.sib_gwas_file_name:
+			self.sib_gwas = pd.read_csv(self.sib_gwas_file_name, delim_whitespace = True, compression = 'gzip')
+		else:
+			self.sib_gwas = pd.read_csv(self.sib_gwas_file_name, delim_whitespace = True)
+		
+		if 'SNP' in self.pop_gwas.columns:
+			self.pop_gwas = self.pop_gwas.drop(columns = ['SNP'])
+			self.pop_gwas['SNP'] = self.pop_gwas[self.chr_label].astype(str) + ':' + self.pop_gwas[self.pos_label].astype(str)
+		else:
+			self.pop_gwas['SNP'] = self.pop_gwas[self.chr_label].astype(str) + ':' + self.pop_gwas[self.pos_label].astype(str)
+
+		self.sib_gwas = self.sib_gwas.drop_duplicates(subset = ['SNP'])
+		self.pop_gwas = self.pop_gwas.drop_duplicates(subset = ['SNP'])
+
+		if snpset:
+			snpset = pd.read_csv(snpset, header = None)
+			snpset.columns = ['SNP']
+		else:
+			snpset = pd.DataFrame(np.zeros((1,2)))
+		
+		self.check_shared_snps(snpset)
+		# print(self.pop_gwas)
+
+	def extract_and_clump(self):
+		
+		if os.path.isfile(self.outdir + '/' + self.outlabel + '.PRS.clumps.snp'):
+			pass
+		else:
+			os.system('PRSice_linux --base ' + outdir + '/' + self.outlabel + '.support.overlap.linear --a1 ' + self.alt_allele + ' --beta --pvalue ' + pval + ' --snp SNP --target ' + self.genetic_file + ' --bar-levels 1 --fastscore --no-regress --clump-r2 0.1 --clump-kb 100 --print-snp --out ' + self.outdir + '/' + self.outlabel + '.PRS.clumps')
+			os.system("""awk 'NR!=1 {print $2}' """ + self.outdir + """/""" + self.outlabtl + """.PRS.clumps.snp > """ + self.outdir +"""/""" + self.outlabel + """.clumped.snps.txt""")
+
+	def check_alt_consensus(self):
+		# self.anc_data['SNP'] = self.anc_data['CHR'].astype(str) + ':' + self.anc_data['POS'].astype(str)
+		relevant_anc = self.anc_data[['SNP','alt.allele']]
+
+		#check effects for to make sure that the alternative allele matches between the summary statistics and the target cohort
+		self.pop_gwas = self.pop_gwas.merge(relevant_anc, on = 'SNP', how = 'inner')
+		matcher = self.sib_gwas[['SNP']].merge(self.pop_gwas[['SNP']], on = 'SNP', how = 'inner')
+		self.pop_gwas = self.pop_gwas[self.pop_gwas['SNP'].isin(matcher['SNP'])].drop_duplicates(subset = ['SNP'])
+		self.sib_gwas = self.sib_gwas[self.sib_gwas['SNP'].isin(matcher['SNP'])].drop_duplicates(subset = ['SNP'])
+		self.pop_gwas = self.pop_gwas.reset_index(drop=True)
+		self.sib_gwas = self.sib_gwas.reset_index(drop=True)
+
+		self.pop_gwas = self.pop_gwas[self.pop_gwas['SNP'].isin(matcher['SNP'])].drop_duplicates(subset = ['SNP'])
+		self.sib_gwas = self.sib_gwas[self.sib_gwas['SNP'].isin(matcher['SNP'])].drop_duplicates(subset = ['SNP'])
+
+		self.pop_gwas['effect_matches'] = (self.pop_gwas[self.alt_allele]==self.pop_gwas['alt.allele']).astype(int)
+		self.pop_gwas['effect_matches'] = 2*(self.pop_gwas['effect_matches']) - 1
+		self.pop_gwas['effect_matches'] = self.pop_gwas['effect_matches'].astype(float)
+		self.pop_gwas['beta.altconsensus'] = self.pop_gwas[self.standard_beta].astype(float) * self.pop_gwas['effect_matches']
+		self.sib_gwas['effect_matches'] = self.pop_gwas['effect_matches']
+		self.sib_gwas['beta.altconsensus'] = self.sib_gwas[self.sib_beta].astype(float) * self.sib_gwas['effect_matches']
+
+	def check_shared_snps(self, preselected_snp_ids):
+		# print()
+		# print(os.path.isfile(self.outdir + '/' + self.outlabel + '.clumped.snps.txt'))
+		# print(preselected_snp_ids.shape[0])
+		self.check_alt_consensus()
+		
+		
+		if os.path.isfile(self.outdir + '/' + self.outlabel + '.support.overlap.linear') and os.path.isfile(self.outdir + '/' + self.outlabel + '.clumped.snps.txt') and preselected_snp_ids.shape[0] == 1:
+
+			clumped_snps = pd.read_csv(self.outdir + '/clumped.snps.txt',sep = '\t', header = None)
+			clumped_snps.columns = ['SNP']
+			if 'BP' not in self.pop_gwas:
+				self.pop_gwas = self.pop_gwas.rename(columns = {self.pos_label:'BP'})
+			
+			self.pop_gwas['ID'] = self.pop_gwas[self.chr_label].astype(str) + ':' + self.pop_gwas['SNP'].astype(str)
+			self.pop_gwas['BP'] = self.pop_gwas['BP'].astype(int).round(decimals=0)
+			self.pop_gwas['SNP'] = self.pop_gwas[self.chr_label].astype(str) + ':' + self.pop_gwas['BP'].astype(str)
+			self.pop_gwas.to_csv(self.outdir + '/' + self.outlabel + '.support.overlap.linear', index = False, sep = '\t')
+			self.pop_gwas = self.pop_gwas[self.pop_gwas['SNP'].isin(clumped_snps['SNP'].tolist())].drop_duplicates()
+			self.sib_gwas['ID'] = self.sib_gwas['CHR'].astype(str) + ':' + self.sib_gwas['SNP'].astype(str)
+			# self.sib_gwas['SNP'] = self.sib_gwas['CHR'].astype(str) + ':' + self.sib_gwas['BP'].astype(str)
+			self.sib_gwas = self.sib_gwas[self.sib_gwas['SNP'].isin(self.pop_gwas['SNP'].tolist())].drop_duplicates()
+			self.pop_gwas = self.pop_gwas.reset_index(drop=True)
+			self.sib_gwas = self.sib_gwas.reset_index(drop=True)
+
+		elif preselected_snp_ids.shape[0] > 1:
+			if 'BP' not in self.pop_gwas:
+				self.pop_gwas = self.pop_gwas.rename(columns = {self.pos_label:'BP'})
+			
+			self.pop_gwas['SNP'] = self.pop_gwas[self.chr_label].astype(str) + ':' + self.pop_gwas['BP'].astype(str)
+			self.pop_gwas = self.pop_gwas.drop_duplicates(subset = ['SNP'])
+
+			self.pop_gwas = self.pop_gwas.merge(preselected_snp_ids, on = 'SNP', how = 'inner')
+			self.pop_gwas.to_csv(self.outdir + '/' + self.outlabel + '.support.overlap.linear', index = False, sep = '\t')
+			self.sib_gwas = self.sib_gwas.merge(preselected_snp_ids, on = 'SNP', how = 'inner')
+			merged = self.pop_gwas.merge(self.sib_gwas, on = 'SNP', how = 'inner')
+			self.pop_gwas = self.pop_gwas.loc[self.pop_gwas['SNP'].isin(merged['SNP'])]
+			self.sib_gwas = self.sib_gwas[self.sib_gwas['SNP'].isin(merged['SNP'])]
+			self.pop_gwas = self.pop_gwas.reset_index(drop=True)
+			self.sib_gwas = self.sib_gwas.reset_index(drop=True)
+
+			self.clump_gwas = self.pop_gwas[[self.chr_label,'SNP','BP', 'A1', 'BETA','T_STAT',pval]]
+			self.clump_gwas = self.clump_gwas.drop_duplicates(subset = 'SNP')
+			# self.clump_gwas.to_csv(self.outdir + '/' + self.outlabel + '.support.overlap.linear', index = False, sep = '\t')
+
+
+		else:
+			print('here')
+			#first make sure that the alternative allele is set to be the same as in the 1kg data
+			#now that we have fixed that let's merge them together, using the chromosome rsid combo label to
+			#account for any potential repetitions of rsIDs on different chromosomes
+			#take the shared SNPs, relabel them, and then write out the list
+			shared_snps = self.pop_gwas[['SNP']].merge(self.sib_gwas[['SNP']], on = 'SNP', how = 'inner')
+			consensus = shared_snps[['SNP']].merge(self.anc_data[['SNP']], on = 'SNP', how = 'inner')
+
+			#now that we have the overlap between snps sets take the corresponding summary statistics from 
+			#the standard GWAS and clump them agnostic of p-value
+			if 'BP' not in self.pop_gwas:
+				self.pop_gwas = self.pop_gwas.rename(columns = {'POS':'BP'})
+			self.pop_gwas = self.pop_gwas.set_index('SNP').loc[consensus['SNP'].tolist()]
+			self.pop_gwas = self.pop_gwas.rename(columns ={'TEST.1':'STAT','OBS_CT':'NIND'})
+			self.pop_gwas = self.pop_gwas.reset_index()
+			self.clump_gwas = self.pop_gwas[[self.chr_label,'SNP','BP', 'A1', 'BETA','T_STAT',pval]]
+			self.clump_gwas = self.clump_gwas.drop_duplicates(subset = 'SNP')
+			self.clump_gwas.to_csv(self.outdir + '/' + self.outlabel + '.support.overlap.linear', index = False, sep = '\t')
+			#extract the consensus from the provided 1kg file and clump them agnostic to p-value
+			self.extract_and_clump()
+
+			#read in the resulting SNPs from each clump
+			clumped_snps = pd.read_csv(self.outdir + '/' + self.outlabel + '.clumped.snps.txt',sep = '\t', header = None)
+			clumped_snps.columns = ['SNP']
+			self.pop_gwas = self.pop_gwas.merge(clumped_snps, on = 'SNP', how = 'inner').drop_duplicates()
+			self.sib_gwas = self.sib_gwas.reset_index(drop = True)
+			# self.sib_gwas['SNP'] = self.sib_gwas['CHR'].astype(str) + ':' + self.sib_gwas['BP'].astype(str)
+			self.sib_gwas = self.sib_gwas[self.sib_gwas['SNP'].isin(self.pop_gwas['SNP'].tolist())]
+
+					
+		self.pop_gwas = self.pop_gwas.rename(columns={'SE':'se'})
+		
+		if '#CHROM' in self.pop_gwas.columns:
+			self.pop_gwas = self.pop_gwas.rename(columns={'#CHROM':'CHR'})
+		
+		if self.log10p:
+			self.pop_gwas['P'] = 10**(-1.0*self.pop_gwas['log10p'])
+			self.sib_gwas['P'] = 10**(-1.0*self.sib_gwas['log10p'])
+
+		self.pop_gwas.to_csv(self.outdir + '/' + self.outlabel + '.standard.preproc.txt', sep = '\t', index = False)
+		siblabel = self.sib_gwas_file_name.replace('.gz','')
+		siblabel = self.sib_gwas_file_name.replace('.txt.gz','')
+			
+		self.sib_gwas.to_csv(self.outdir + '/' + self.outlabel + '.sib.preproc.txt', sep = '\t', index = False)
+
+if __name__ == '__main__':
+	x = make_input_files(outdir, outlabel, popgwas, sibgwasperm, genetic_file, ancdata, chrom, pos, snpid, trait,
+	 alt_allele, standard_beta, sib_beta, log10p, snpset)
+	print('Munge complete.')
+
+
